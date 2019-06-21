cf013b7c7928892759fd89345700c497f2eb49e1
alexander.parovenko@devoxsoftware.com <alexander.parovenko@devoxsoftware.com>
2019-06-21 21:39:22 +0300

    Zeit govno iz zopi

--- startup.js
+++ startup.js
@@ -8,6 +8,7 @@ class Startup {
 
    setupTelegramBot() {
       this._bot = new TelegramBot(process.env.TOKEN);
+      console.log(process.env.TOKEN);
       this._bot.setWebHook(this._config.telegram.webhookUrl);
       this._bot.on('message', msg => {
          bot.sendMessage(msg.chat.id, 'I am alive!');
